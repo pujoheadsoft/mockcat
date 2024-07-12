a5edd545637d64590fdac449cdf396ec8729ba33
Pujo <pujoheadsoft@gmail.com>
2024-07-12 23:00:24 +0900

    wip

--- src/Test/MockCat.hs
+++ src/Test/MockCat.hs
@@ -2,9 +2,14 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Test.MockCat where
 import Test.MockCat.Param
 import Test.MockCat.Cons
+import Data.Maybe
+import Data.List (intercalate)
+import Test.MockCat.ParamDivider
 
 data Mock fun params = Mock (Maybe MockName) fun
 type MockName = String
@@ -22,20 +27,56 @@ class MockBuilder params fun verifyParams | params -> fun, params -> verifyParam
 -- createMock :: Eq params => Show params => Maybe MockName -> CalledParamsList params -> fun -> IO (Mock fun params)
 -- createMock name l fn = pure $ Mock name fn
 
--- type CalledParamsList params = [params]
+type CalledParamsList params = [params]
 
--- extractReturnValueWithValidate ::
---      ParamDivider params args (Param r)
---   => Eq args
---   => Show args
---   => Maybe MockName
---   -> params
---   -> args
---   -> CalledParamsStore args
---   -> r
--- extractReturnValueWithValidate name params inputParams s =
---   let
---     expectedArgs = args params
---     r = returnValue params
---     _ = validateWithStoreParams name s expectedArgs inputParams
---   in r
+extractReturnValueWithValidate ::
+     ParamDivider params args (Param r)
+  => Eq args
+  => Show args
+  => Maybe MockName
+  -> params
+  -> args
+  -> CalledParamsStore args
+  -> r
+extractReturnValueWithValidate name params inputParams s =
+  let
+    expectedArgs = args params
+    r = returnValue params
+    _ = validateWithStoreParams name s expectedArgs inputParams
+  in r
+
+validateWithStoreParams :: Eq a => Show a => Maybe MockName -> CalledParamsStore a -> a -> a -> ()
+validateWithStoreParams name s expected actual = validateParams name expected (storeCalledParams s actual)
+
+data CalledParamsStore params = CalledParamsStore {
+  calledParamsList :: CalledParamsList params,
+  store :: params -> IO ()
+}
+
+storeCalledParams :: CalledParamsStore a -> a -> a
+storeCalledParams (CalledParamsStore { .. }) a = const a (store a)
+
+validateParams :: Eq a => Show a => Maybe MockName -> a -> a -> ()
+validateParams name expected actual =
+  if expected == actual then ()
+  else error $ message name expected actual
+
+
+{-
+  Function was not called with expected arguments.
+  expected: 1, "2", 3
+  but was : 1, "1", 1
+-}
+message :: Show a => Maybe MockName -> a -> a -> String
+message name expected actual =
+  intercalate "\n" [
+    "function" <> mockNameLabel name <> "was not called with expected arguments.",
+    "  expected: " <> show expected,
+    "  but was : " <> show actual
+  ]
+
+mockNameLabel :: Maybe MockName -> String
+mockNameLabel = fromMaybe " " . enclose " " . enclose "`"
+
+enclose :: String -> Maybe String -> Maybe String
+enclose e = fmap (\v -> e <> v <> e)
