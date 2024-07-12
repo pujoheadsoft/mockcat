706b1b2de53e4ef38bb0c0ff2f2f01c53b13a623
Pujo <pujoheadsoft@gmail.com>
2024-07-13 00:52:40 +0900

    wip

--- src/Test/MockCat.hs
+++ src/Test/MockCat.hs
@@ -5,15 +5,34 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Test.MockCat where
-import Test.MockCat.Param
+import Test.MockCat.Param hiding (any)
 import Test.MockCat.Cons
 import Data.Maybe
-import Data.List (intercalate)
+import Data.List (intercalate, all)
 import Test.MockCat.ParamDivider
+import Data.IORef (newIORef, IORef, modifyIORef', readIORef)
+import GHC.IO (unsafePerformIO)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Function ((&))
+import Control.Monad (guard)
+import Data.Text (unpack, replace, pack)
 
 data Mock fun params = Mock (Maybe MockName) fun (Verifier params)
 type MockName = String
-newtype Verifier params = Verifier (CalledParamsList params)
+newtype Verifier params = Verifier (IORef (CalledParamsList params))
+
+
+mock :: MockBuilder params fun verifyParams
+  => MonadIO m
+  => params
+  -> m (Mock fun verifyParams)
+mock params = liftIO $ build Nothing params
+
+fun :: Mock fun v -> fun
+fun (Mock _ f _) = f
+
+callParamsList :: MonadIO m => Mock fun v -> m (CalledParamsList v)
+callParamsList (Mock _ _ (Verifier ref)) = liftIO $ readIORef ref
 
 class MockBuilder params fun verifyParams | params -> fun, params -> verifyParams where
   build :: Maybe MockName -> params -> IO (Mock fun verifyParams)
@@ -22,8 +41,8 @@ class MockBuilder params fun verifyParams | params -> fun, params -> verifyParam
 instance (Show a, Eq a)
   => MockBuilder (Param a :> Param r) (a -> r) (Param a) where
   build name params = do
-    s <- _store
-    createMock name (calledParamsList s) (\a2 -> extractReturnValueWithValidate name params (p a2) s)
+    s <- newIORef ([] :: CalledParamsList params)
+    createMock name s (\a2 -> unsafePerformIO $ extractReturnValueWithValidate name params (p a2) s)
 
 p :: a -> Param a
 p = param
@@ -31,7 +50,7 @@ p = param
 _store :: IO (CalledParamsStore params)
 _store = undefined
 
-createMock :: Eq params => Show params => Maybe MockName -> CalledParamsList params -> fun -> IO (Mock fun params)
+createMock :: Maybe MockName -> IORef (CalledParamsList params) -> fun -> IO (Mock fun params)
 createMock name l fn = pure $ Mock name fn (Verifier l)
 
 type CalledParamsList params = [params]
@@ -43,25 +62,26 @@ extractReturnValueWithValidate ::
   => Maybe MockName
   -> params
   -> args
-  -> CalledParamsStore args
-  -> r
-extractReturnValueWithValidate name params inputParams s =
-  let
-    expectedArgs = args params
-    r = returnValue params
-    _ = validateWithStoreParams name s expectedArgs inputParams
-  in r
+  -> IORef (CalledParamsList args)
+  -> IO r
+extractReturnValueWithValidate name params inputParams s = do
+  validateWithStoreParams name s (args params) inputParams
+  pure $ returnValue params
 
-validateWithStoreParams :: Eq a => Show a => Maybe MockName -> CalledParamsStore a -> a -> a -> ()
-validateWithStoreParams name s expected actual = validateParams name expected (storeCalledParams s actual)
+validateWithStoreParams :: Eq a => Show a => Maybe MockName -> IORef (CalledParamsList a) -> a -> a -> IO ()
+validateWithStoreParams name s expected actual = do
+  a <- storeCalledParams s actual
+  pure $ validateParams name expected a
 
 data CalledParamsStore params = CalledParamsStore {
   calledParamsList :: CalledParamsList params,
   store :: params -> IO ()
 }
 
-storeCalledParams :: CalledParamsStore a -> a -> a
-storeCalledParams (CalledParamsStore { .. }) a = const a (store a)
+storeCalledParams :: IORef (CalledParamsList a) -> a -> IO a
+storeCalledParams ref a = do
+  modifyIORef' ref (++ [a])
+  pure a
 
 validateParams :: Eq a => Show a => Maybe MockName -> a -> a -> ()
 validateParams name expected actual =
@@ -87,3 +107,51 @@ mockNameLabel = fromMaybe " " . enclose " " . enclose "`"
 
 enclose :: String -> Maybe String -> Maybe String
 enclose e = fmap (\v -> e <> v <> e)
+
+-- verify
+data VerifyMatchType a = MatchAny a | MatchAll a
+
+class Verify params input where
+  verify :: Mock fun params -> input -> IO ()
+
+instance (Eq a, Show a) => Verify (Param a) a where
+  verify v a = _verify v (MatchAny (param a))
+
+-- instance (Eq a, Show a) => Verify a a where
+--   verify v a = _verify v (MatchAny a)
+
+_verify :: Eq params => Show params => Mock fun params -> VerifyMatchType params -> IO ()
+_verify (Mock name _ (Verifier ref)) matcher = do
+  calledParamsList <- liftIO $ readIORef ref
+  case doVerify name calledParamsList matcher of
+    Just (VerifyFailed msg) -> fail msg
+    Nothing -> pure ()
+
+newtype VerifyFailed = VerifyFailed Message
+type Message = String
+
+doVerify :: Eq a => Show a => Maybe MockName -> CalledParamsList a -> VerifyMatchType a -> Maybe VerifyFailed
+doVerify name list (MatchAny a) = do
+  guard $ notElem a list
+  pure $ verifyFailedMesssage name list a
+doVerify name list (MatchAll a) = do
+  guard $ any (a /=) list
+  pure $ verifyFailedMesssage name list a
+
+verifyFailedMesssage :: Show a => Maybe MockName -> CalledParamsList a -> a -> VerifyFailed
+verifyFailedMesssage name calledParams expected =
+  VerifyFailed $ intercalate "\n"
+    ["function" <> mockNameLabel name <> "was not called with expected arguments.",
+     "  expected: " <> show expected,
+     "  but was : " <> formatCalledParamsList calledParams]
+
+formatCalledParamsList :: Show a => CalledParamsList a -> String
+formatCalledParamsList calledParams
+  | length calledParams == 0 =
+    "Never been called."
+  | length calledParams == 1 =
+    show $ (_replace "[" . _replace "]") calledParams
+  | otherwise = show calledParams
+
+_replace :: Show a => String -> a -> String
+_replace r s = unpack $ replace (pack r) (pack "") (pack (show s))
---
+++ test/Test/MockCatSpec.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+module Test.MockCatSpec (spec) where
+
+import Test.Hspec
+import Test.MockCat
+import Test.MockCat.Param
+import Test.MockCat
+
+
+spec :: Spec
+spec = do
+  describe "mock" do
+    it "fn" do
+      m <- mock $ "a" |> "x"
+      -- let 
+      --   f = fun m
+      --   v = f "a"
+      -- v `shouldBe` "x"
+      verify m "a"
