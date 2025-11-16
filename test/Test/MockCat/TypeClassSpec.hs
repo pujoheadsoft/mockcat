36af92ea3ae11b140275193c366a6519243b77a1
Pujo <pujoheadsoft@gmail.com>
2025-11-16 14:23:59 +0900

    wip

--- test/Test/MockCat/TypeClassSpec.hs
+++ test/Test/MockCat/TypeClassSpec.hs
@@ -3,9 +3,10 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
@@ -27,6 +28,8 @@ import Control.Monad.Reader (MonadReader)
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Reader.Class (ask, MonadReader (local))
 import Control.Monad.Trans.Class (lift)
+import Control.Monad.State (MonadState (..), StateT, evalStateT)
+import Control.Monad.IO.Unlift (MonadUnliftIO)
 import qualified Test.MockCat.Verify as Verify
 import Test.MockCat.Internal.Types (Verifier (..))
 import Test.MockCat.Internal.Message (mockNameLabel)
@@ -88,6 +91,11 @@ instance MonadIO m => MonadReader String (MockT m) where
       !result = mock
     pure result
   local = undefined
+
+instance MonadState s m => MonadState s (MockT m) where
+  get = lift get
+  put = lift . put
+  state f = lift (state f)
 
 _ask ::
   ( Typeable env
@@ -168,6 +176,12 @@ echoProgram s = do
   liftIO $ print v
   echo $ show v
 
+class (Eq s, Show s, MonadState s m) => MonadStateSub s m where
+  fnState :: Maybe s -> m s
+
+class (MonadState String m) => MonadStateSub2 s m where
+  fnState2 :: String -> m ()
+
 class Monad m => MonadVar2_1 m a where
 class MonadVar2_1 m a => MonadVar2_1Sub m a where
   fn2_1Sub :: String -> m ()
@@ -194,6 +208,18 @@ class Monad m => MultiApplyTest m where
 getValues :: MultiApplyTest m => [String] -> m [String]
 getValues = mapM getValueBy
 
+class Monad m => ExplicitlyReturnMonadicValuesTest m where
+  echoExplicit :: String -> m ()
+  getByExplicit :: String -> m Int
+
+class Monad m => ParamThreeMonad a b m | m -> a, m -> b where
+  fnParam3_1 :: a -> b -> m String
+  fnParam3_2 :: m a
+  fnParam3_3 :: m b
+
+class MonadUnliftIO m => MonadAsync m where
+  mapConcurrently :: Traversable t => (a -> m b) -> t a -> m (t b)
+
 instance MonadIO m => TestClass (MockT m) where
   getBy a = MockT do
     defs <- getDefinitions
@@ -207,6 +233,34 @@ instance MonadIO m => TestClass (MockT m) where
     let
       mockFn = fromMaybe (error "no answer found stub function `_echo`.") $ findParam (Proxy :: Proxy "_echo") defs
       !result = mockFn a
+    lift result
+
+instance
+  ( Eq s
+  , Show s
+  , MonadState s m
+  , MonadIO m
+  ) =>
+  MonadStateSub s (MockT m)
+  where
+  fnState maybeValue = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_fnState`.") $ findParam (Proxy :: Proxy "_fnState") defs
+      !result = mockFn maybeValue
+    lift result
+
+instance
+  ( MonadState String m
+  , MonadIO m
+  ) =>
+  MonadStateSub2 s (MockT m)
+  where
+  fnState2 label = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_fnState2`.") $ findParam (Proxy :: Proxy "_fnState2") defs
+      !result = mockFn label
     lift result
 
 instance Monad m => MonadVar2_1 (MockT m) a
@@ -261,6 +315,43 @@ instance MonadIO m => MultiApplyTest (MockT m) where
       mockFn = fromMaybe (error "no answer found stub function `_getValueBy`.") $ findParam (Proxy :: Proxy "_getValueBy") defs
       !result = mockFn key
     lift result
+
+instance MonadIO m => ExplicitlyReturnMonadicValuesTest (MockT m) where
+  getByExplicit label = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_getByExplicit`.") $ findParam (Proxy :: Proxy "_getByExplicit") defs
+      !result = mockFn label
+    lift result
+  echoExplicit label = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_echoExplicit`.") $ findParam (Proxy :: Proxy "_echoExplicit") defs
+      !result = mockFn label
+    lift result
+
+instance MonadIO m => ParamThreeMonad Int Bool (MockT m) where
+  fnParam3_1 a b = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_fnParam3_1`.") $ findParam (Proxy :: Proxy "_fnParam3_1") defs
+      !result = mockFn a b
+    lift result
+  fnParam3_2 = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_fnParam3_2`.") $ findParam (Proxy :: Proxy "_fnParam3_2") defs
+      !result = mockFn
+    lift result
+  fnParam3_3 = MockT do
+    defs <- getDefinitions
+    let
+      mockFn = fromMaybe (error "no answer found stub function `_fnParam3_3`.") $ findParam (Proxy :: Proxy "_fnParam3_3") defs
+      !result = mockFn
+    lift result
+
+instance (MonadUnliftIO m) => MonadAsync (MockT m) where
+  mapConcurrently = traverse
 
 _getBy ::
   ( MockBuilder params (String -> m Int) (Param String)
@@ -406,6 +497,142 @@ _getValueBy p = MockT $ do
   let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
   addDefinition (Definition (Proxy :: Proxy "_getValueBy") mockInstance verifyStub)
 
+_fnState ::
+  ( MockBuilder params (Maybe s -> m s) (Param (Maybe s))
+  , MonadIO m
+  , Typeable m
+  , Typeable s
+  , Verify.ResolvableParamsOf (Maybe s -> m s) ~ Param (Maybe s)
+  ) =>
+  params ->
+  MockT m ()
+_fnState p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_fnState" p
+  let verifyStub _ = pure ()
+  addDefinition (Definition (Proxy :: Proxy "_fnState") mockInstance verifyStub)
+
+_fnState2 ::
+  ( MockBuilder params (String -> m ()) (Param String)
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (String -> m ()) ~ Param String
+  ) =>
+  params ->
+  MockT m ()
+_fnState2 p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_fnState2" p
+  let verifyStub _ = pure ()
+  addDefinition (Definition (Proxy :: Proxy "_fnState2") mockInstance verifyStub)
+
+_fnParam3_1 ::
+  ( MockBuilder params (Int -> Bool -> m String) (Param Int :> Param Bool)
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (Int -> Bool -> m String) ~ (Param Int :> Param Bool)
+  ) =>
+  params ->
+  MockT m ()
+_fnParam3_1 p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_fnParam3_1" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "_fnParam3_1") mockInstance verifyStub)
+
+_fnParam3_2 ::
+  ( MockBuilder params (m Int) ()
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (m Int) ~ ()
+  ) =>
+  params ->
+  MockT m ()
+_fnParam3_2 p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_fnParam3_2" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "_fnParam3_2") mockInstance verifyStub)
+
+_fnParam3_3 ::
+  ( MockBuilder params (m Bool) ()
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (m Bool) ~ ()
+  ) =>
+  params ->
+  MockT m ()
+_fnParam3_3 p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_fnParam3_3" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "_fnParam3_3") mockInstance verifyStub)
+
+_getByExplicit ::
+  ( MockBuilder params (String -> m Int) (Param String)
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (String -> m Int) ~ Param String
+  ) =>
+  params ->
+  MockT m ()
+_getByExplicit p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_getByExplicit" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "_getByExplicit") mockInstance verifyStub)
+
+_echoExplicit ::
+  ( MockBuilder params (String -> m ()) (Param String)
+  , MonadIO m
+  , Typeable m
+  , Verify.ResolvableParamsOf (String -> m ()) ~ Param String
+  ) =>
+  params ->
+  MockT m ()
+_echoExplicit p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "_echoExplicit" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "_echoExplicit") mockInstance verifyStub)
+
+stubPostFn ::
+  ( MockBuilder params (Text -> ()) (Param Text)
+  , MonadIO m
+  ) =>
+  params ->
+  MockT m ()
+stubPostFn p = MockT $ do
+  mockInstance <- liftIO $ createNamedStubFn "stubPostFn" p
+  resolved <- liftIO $ do
+    result <- Verify.resolveForVerification mockInstance
+    case result of
+      Just (maybeName, verifier) -> pure $ Verify.ResolvedMock maybeName verifier
+      Nothing -> Verify.verificationFailure
+  let verifyStub _ = Verify.shouldApplyToAnythingResolved resolved
+  addDefinition (Definition (Proxy :: Proxy "post") mockInstance verifyStub)
+
+processFiles :: (MonadAsync m, FileOperation m) => [FilePath] -> m [Text]
+processFiles = mapConcurrently readFile
+
 
 class Monad m => Teletype m where
   readTTY :: m String
@@ -539,3 +766,54 @@ spec = do
       _fn3_3Sub $ "epsilon" |> pure @IO ()
       fn3_3Sub "epsilon"
     result `shouldBe` ()
+
+  it "supports MonadStateSub pattern" do
+    let action = runMockT $ do
+          _fnState $ do
+            onCase $ Just "current" |> pure @(StateT String IO) "next"
+            onCase $ Nothing |> pure @(StateT String IO) "default"
+          fnState (Just "current")
+    result <- evalStateT action "seed"
+    result `shouldBe` "next"
+
+  it "supports MonadStateSub2 pattern" do
+    let action = runMockT $ do
+          _fnState2 $ "label" |> pure @(StateT String IO) ()
+          fnState2 @String "label"
+    result <- evalStateT action "initial"
+    result `shouldBe` ()
+
+  it "supports ParamThreeMonad functional dependencies" do
+    result <- runMockT $ do
+      _fnParam3_1 $ do
+        onCase $ (1 :: Int) |> True |> pure @IO "combined"
+      _fnParam3_2 $ casesIO [1 :: Int]
+      _fnParam3_3 $ casesIO [True]
+      r1 <- fnParam3_1 1 True
+      r2 <- fnParam3_2
+      r3 <- fnParam3_3
+      pure (r1, r2, r3)
+    result `shouldBe` ("combined", 1, True)
+
+  it "supports ExplicitlyReturnMonadicValuesTest pattern" do
+    result <- runMockT $ do
+      _getByExplicit $ "key" |> pure @IO (42 :: Int)
+      _echoExplicit $ "value" |> pure @IO ()
+      v <- getByExplicit "key"
+      echoExplicit "value"
+      pure v
+    result `shouldBe` 42
+
+  it "supports makeMockWithOptions prefix and suffix" do
+    result <- runMockT $ do
+      stubPostFn (pack "payload" |> ())
+      post (pack "payload")
+    result `shouldBe` ()
+
+  it "supports MonadAsync pattern" do
+    result <- runMockT $ do
+      _readFile $ do
+        onCase $ "file1.txt" |> pack "content1"
+        onCase $ "file2.txt" |> pack "content2"
+      processFiles ["file1.txt", "file2.txt"]
+    result `shouldBe` [pack "content1", pack "content2"]
