55ba6bdf58631fba029c55e7dfd65f89cef68300
Pujo <pujoheadsoft@gmail.com>
2025-12-08 20:31:38 +0900

    wip

--- test/Test/MockCat/TypeClassTHSpec.hs
+++ test/Test/MockCat/TypeClassTHSpec.hs
@@ -18,75 +18,27 @@
 module Test.MockCat.TypeClassTHSpec (spec) where
 
 import Prelude hiding (readFile, writeFile, any)
-import Data.Text (Text, pack, isInfixOf)
 import Test.Hspec
 import Test.MockCat
-import Control.Monad.State
-import Control.Monad.Reader (MonadReader, ask)
-import Data.Proxy (Proxy(..))
-import Control.Monad (unless)
-import Control.Monad.IO.Unlift (withRunInIO, MonadUnliftIO)
-import Control.Concurrent.Async (async, wait)
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Test.MockCat.SharedSpecDefs
 import qualified Test.MockCat.TypeClassCommonSpec as SpecCommon
-import qualified Test.MockCat.Verify as Verify
-
-operationProgram ::
-  FileOperation m =>
-  FilePath ->
-  FilePath ->
-  m ()
-operationProgram inputPath outputPath = do
-  content <- readFile inputPath
-  unless (pack "ngWord" `isInfixOf` content) $
-    writeFile outputPath content
-
-operationProgram2 ::
-  (FileOperation m, ApiOperation m) =>
-  FilePath ->
-  FilePath ->
-  (Text -> Text) ->
-  m ()
-operationProgram2 inputPath outputPath modifyText = do
-  content <- readFile inputPath
-  let modifiedContent = modifyText content
-  writeFile outputPath modifiedContent
-  post modifiedContent
-
-operationProgram3 ::
-  MonadReader SpecCommon.Environment m =>
-  FileOperation m =>
-  m ()
-operationProgram3 = do
-  (SpecCommon.Environment inputPath outputPath) <- ask
-  content <- readFile inputPath
-  writeFile outputPath content
-
 
 --makeMock [t|MonadReader Bool|]
 makeMock [t|MonadReader SpecCommon.Environment|]
-makeMockWithOptions [t|MonadVar2_1Sub|] options { implicitMonadicReturn = True }
-makeMockWithOptions [t|MonadVar2_2Sub|] options { implicitMonadicReturn = True }
-makeMockWithOptions [t|MonadVar3_1Sub|] options { implicitMonadicReturn = True }
-makeMockWithOptions [t|MonadVar3_2Sub|] options { implicitMonadicReturn = True }
-makeMockWithOptions [t|MonadVar3_3Sub|] options { implicitMonadicReturn = True }
+makeMockWithOptions [t|MonadVar2_1Sub|] options { implicitMonadicReturn = False }
+makeMockWithOptions [t|MonadVar2_2Sub|] options { implicitMonadicReturn = False }
+makeMockWithOptions [t|MonadVar3_1Sub|] options { implicitMonadicReturn = False }
+makeMockWithOptions [t|MonadVar3_2Sub|] options { implicitMonadicReturn = False }
+makeMockWithOptions [t|MonadVar3_3Sub|] options { implicitMonadicReturn = False }
 makeMock [t|FileOperation|]
 makeMock [t|ApiOperation|]
-
 makeMockWithOptions [t|MultiApplyTest|] options { implicitMonadicReturn = False }
-
 makeMockWithOptions [t|ParamThreeMonad Int Bool|] options { implicitMonadicReturn = False }
-
 makeMockWithOptions [t|MonadStateSub|] options { implicitMonadicReturn = False }
 makeMockWithOptions [t|MonadStateSub2|] options { implicitMonadicReturn = False }
-
 makeMockWithOptions [t|Teletype|] options { implicitMonadicReturn = False }
-
-echoProgram :: ExplicitlyReturnMonadicValuesTest m => String -> m ()
-echoProgram s = do
-  v <- getByExplicit s
-  echoExplicit $ show v
-
 makeMockWithOptions [t|ExplicitlyReturnMonadicValuesTest|] options { implicitMonadicReturn = False }
 makeMock [t|DefaultMethodTest|]
 makeMock [t|AssocTypeTest|]
@@ -98,70 +50,6 @@ instance (MonadUnliftIO m) => MonadAsync (MockT m) where
 instance AssocTypeTest IO where
   type ResultType IO = Int
   produce = pure 0
-
-processFiles :: MonadAsync m => FileOperation m => [FilePath] -> m [Text]
-processFiles = mapConcurrently readFile
-
- 
-
- 
-
-ensureVerifiable ::
-  ( MonadIO m
-  , Verify.ResolvableMock target
-  ) =>
-  target ->
-  m ()
-ensureVerifiable target =
-  liftIO $ do
-    m <- Verify.resolveForVerification target
-    case m of { Just _ -> pure (); Nothing -> Verify.verificationFailure }
-
--- IO wrappers for subvars (TH generates pure-returning builders here)
-_fn2_1SubIO ::
-  forall params.
-  (MockBuilder params (String -> IO ()) (Param String)) =>
-  params -> MockT IO (String -> IO ())
-_fn2_1SubIO p = MockT $ do
-  mockInstance <- liftIO $ createNamedMockFnWithParams "_fn2_1Sub" p
-  addDefinition (Definition (Proxy :: Proxy "_fn2_1Sub") mockInstance NoVerification)
-  pure mockInstance
-
-_fn2_2SubIO ::
-  forall params.
-  (MockBuilder params (String -> IO ()) (Param String)) =>
-  params -> MockT IO (String -> IO ())
-_fn2_2SubIO p = MockT $ do
-  mockInstance <- liftIO $ createNamedMockFnWithParams "_fn2_2Sub" p
-  addDefinition (Definition (Proxy :: Proxy "_fn2_2Sub") mockInstance NoVerification)
-  pure mockInstance
-
-_fn3_1SubIO ::
-  forall params.
-  (MockBuilder params (String -> IO ()) (Param String)) =>
-  params -> MockT IO (String -> IO ())
-_fn3_1SubIO p = MockT $ do
-  mockInstance <- liftIO $ createNamedMockFnWithParams "_fn3_1Sub" p
-  addDefinition (Definition (Proxy :: Proxy "_fn3_1Sub") mockInstance NoVerification)
-  pure mockInstance
-
-_fn3_2SubIO ::
-  forall params.
-  (MockBuilder params (String -> IO ()) (Param String)) =>
-  params -> MockT IO (String -> IO ())
-_fn3_2SubIO p = MockT $ do
-  mockInstance <- liftIO $ createNamedMockFnWithParams "_fn3_2Sub" p
-  addDefinition (Definition (Proxy :: Proxy "_fn3_2Sub") mockInstance NoVerification)
-  pure mockInstance
-
-_fn3_3SubIO ::
-  forall params.
-  (MockBuilder params (String -> IO ()) (Param String)) =>
-  params -> MockT IO (String -> IO ())
-_fn3_3SubIO p = MockT $ do
-  mockInstance <- liftIO $ createNamedMockFnWithParams "_fn3_3Sub" p
-  addDefinition (Definition (Proxy :: Proxy "_fn3_3Sub") mockInstance NoVerification)
-  pure mockInstance
 
 spec :: Spec
 spec = do
@@ -180,8 +68,8 @@ spec = do
         , SpecCommon.multiApplyDeps               = SpecCommon.MultiApplyDeps _getValueBy
         , SpecCommon.monadStateTransformerDeps    = SpecCommon.MonadStateTransformerDeps _fnState _fnState2
         , SpecCommon.stateDeps                    = SpecCommon.StateDeps _fnState _fnState2
-        , SpecCommon.multiParamTypeClassArityDeps = SpecCommon.MultiParamTypeClassArityDeps _fn2_1SubIO _fn2_2SubIO _fn3_1SubIO _fn3_2SubIO _fn3_3SubIO
-        , SpecCommon.multiParamDeps               = SpecCommon.MultiParamDeps _fn2_1SubIO _fn2_2SubIO _fn3_1SubIO _fn3_2SubIO _fn3_3SubIO
+        , SpecCommon.multiParamTypeClassArityDeps = SpecCommon.MultiParamTypeClassArityDeps _fn2_1Sub _fn2_2Sub _fn3_1Sub _fn3_2Sub _fn3_3Sub
+        , SpecCommon.multiParamDeps               = SpecCommon.MultiParamDeps _fn2_1Sub _fn2_2Sub _fn3_1Sub _fn3_2Sub _fn3_3Sub
         , SpecCommon.functionalDependenciesDeps   = SpecCommon.FunctionalDependenciesDeps _fnParam3_1 _fnParam3_2 _fnParam3_3
         , SpecCommon.funDeps                      = SpecCommon.FunDeps _fnParam3_1 _fnParam3_2 _fnParam3_3
         , SpecCommon.explicitMonadicReturnDeps    = SpecCommon.ExplicitMonadicReturnDeps _getByExplicit _echoExplicit
